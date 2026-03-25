6bf6fdf8ba3d4a6a0c00013dc407b53019a48498
ofekda99 <ofekda9@gmail.com>
2026-03-25 18:18:30 +0200

    Fix import statement for NavBar component

--- smart-delivery-frontend/src/App.jsx
+++ smart-delivery-frontend/src/App.jsx
@@ -89,7 +89,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 // import customized components
-//import Navbar from './Components/NavBar';
+//import Navbar from './Components/navbar';
 import NavBar from "./components/NavBar";
 import ProtectedRoute from './Components/ProtectedRoute';
 
